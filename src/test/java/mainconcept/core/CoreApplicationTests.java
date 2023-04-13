65609baccf5c97f9042fe7cf88450c044539cdac
timber3 <timber3@naver.com>
2023-04-13 22:45:41 +0900

    04.13 first commit

---
+++ src/test/java/mainconcept/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package mainconcept.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
