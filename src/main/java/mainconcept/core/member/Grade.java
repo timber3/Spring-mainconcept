3ad919daf15cd4dc75d022157c943fca7a507419
timber3 <timber3@naver.com>
2023-04-14 23:33:07 +0900

    04.14 Member 도메인 + MemberRepository(Memory) + MemberServiceImpl

---
+++ src/main/java/mainconcept/core/member/Grade.java
@@ -0,0 +1,6 @@
+package mainconcept.core.member;
+
+public enum Grade {
+    BASIC,
+    VIP
+}
---
+++ src/main/java/mainconcept/core/member/MemoryMemberRepository.java
@@ -0,0 +1,22 @@
+package mainconcept.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // 실무에서는 ConcurrentHashMap을 사용해야한다 (동시성 이슈)
+    private static Map<Long, Member> store = new HashMap<>();
+
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+
+}
